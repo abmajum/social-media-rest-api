c28a19f6b3d41afdd2899dda55a2146d628a7c1a
Abhishek Majumdar <abhinitp2k12@gmail.com>
2024-11-25 20:50:51 +0530

    Updated schema and created basic post method

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randint
 
 app = FastAPI()
+
+my_posts = [
+    {"title": "Favorite beaches", "content": "Puri, Seven mile, Eagle beach, Kannapali Beach", "id": 1},
+    {"title": "Favorite Books", "content": "Harry potter series, Hunger games,", "id": 2},
+    {"title": "Games for life", "content": "I will play AC series again and again", "id": 3}
+]
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = False
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
@@ -14,8 +24,11 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is my post"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(payload: Post):
-    return{"title": payload.title , "content": payload.content}
+    payload = payload.model_dump()
+    payload["id"] = randint(4,10000)
+    my_posts.append(payload)
+    return{"data": my_posts}
